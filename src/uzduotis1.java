e95dd7ec4fbb630397eaee645c36ee57e59e4be1
Tomaszius <liutkus.to@gmail.com>
2022-06-28 15:02:10 +0300

    sukurtas naujas ciklu masyvu uzduociu rep

---
+++ src/uzduotis1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class uzduotis1 {
+    public static void main(String[] args) {
+
+
+        Scanner skaitytuvas = new Scanner(System.in);
+
+
+        public static int lopas(skaitytuvas){
+        int skaicius = skaitytuvas.nextInt();
+        }
+        return skaicius;
+    }
+}
